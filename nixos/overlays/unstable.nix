8c9c778c66e51678aa871db9f57859c848a60842
Andrei Lapshin <alapshin@fastmail.com>
2018-11-19 12:58:02 +0300

    Fix android-studio overrides in unstable overlay

--- nixos/overlays/unstable.nix
+++ nixos/overlays/unstable.nix
@@ -3,6 +3,7 @@ self: super:
 let 
   unstableRepo = fetchGit {
     ref = "master";
+    rev = "f6169667efaa4e5d55d3b2060475d791d753f174";
     url = https://github.com/NixOS/nixpkgs.git;
   };
   unstablePkgs = import unstableRepo { config = super.config; overlays = []; };
@@ -15,5 +16,8 @@ in
   jetbrains.clion = unstablePkgs.jetbrains.clion;
   jetbrains.idea-ultimate = unstablePkgs.jetbrains.idea-ultimate;
   jetbrains.pycharm-professional = unstablePkgs.jetbrains.pycharm-professional;
+  androidStudioPackages.stable = unstablePkgs.androidStudioPackages.beta;
   androidStudioPackages.beta = unstablePkgs.androidStudioPackages.beta;
+  androidStudioPackages.dev = unstablePkgs.androidStudioPackages.beta;
+  androidStudioPackages.canary = unstablePkgs.androidStudioPackages.beta;
 }
