b9336cee8c26096fef72b2d236f93a702e37afcc
Andrei Lapshin <alapshin@fastmail.com>
2018-12-09 16:03:00 +0300

    Install skrooge from git to get needed bugfixes

--- config/overlays/unstable.nix
+++ config/overlays/unstable.nix
@@ -8,11 +8,20 @@ let
   unstablePkgs = import unstableRepo { config = self.config; overlays = []; };
 in
 {
-  skrooge = unstablePkgs.skrooge;
   home-manager = unstablePkgs.home-manager;
 
   jetbrains = unstablePkgs.jetbrains;
   androidStudioPackages = unstablePkgs.androidStudioPackages;
+
+  skrooge = unstablePkgs.skrooge.overrideAttrs (oldAttrs: rec {
+    name = "skrooge-${version}";
+    version = "2.17.0";
+    src = self.fetchgit {
+      url = "https://anongit.kde.org/skrooge.git";
+      rev = "b6cff4bad4c0875ad3b974684cb298b4253388f0";
+      sha256 = "11sbrl9868igckbmy6b29rkljga45pqv0ds9y0c28azsdgvmh2jq";
+    };
+  });
 
   python36Packages = super.python36Packages // {
     tensorflowWithCuda = unstablePkgs.python36Packages.tensorflowWithCuda;
