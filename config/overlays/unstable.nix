0ad25c375b58e65b122ee8bd020b67103d66ccfd
Andrei Lapshin <alapshin@fastmail.com>
2018-12-20 09:43:03 +0300

    Update skrooge

--- config/overlays/unstable.nix
+++ config/overlays/unstable.nix
@@ -15,11 +15,11 @@ in
 
   skrooge = unstablePkgs.skrooge.overrideAttrs (oldAttrs: rec {
     name = "skrooge-${version}";
-    version = "2.17.0";
+    version = "2.18.0";
     src = self.fetchgit {
       url = "https://anongit.kde.org/skrooge.git";
-      rev = "b6cff4bad4c0875ad3b974684cb298b4253388f0";
-      sha256 = "11sbrl9868igckbmy6b29rkljga45pqv0ds9y0c28azsdgvmh2jq";
+      rev = "94102bbfff76ee92bd02a45edabcb430f8d8a525";
+      sha256 = "0a1b44jbhgzrhmxcxzk6d4j2hcsqv5kbqbkyrnqgmzm1b6zws43z";
     };
   });
 }
