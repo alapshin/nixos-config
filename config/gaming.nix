49a33942fec9434416d737b04be7ff5ac4fc40d2
Andrei Lapshin <alapshin@fastmail.com>
2019-02-14 09:44:52 +0300

    Use wine-wow build as default

--- config/gaming.nix
+++ config/gaming.nix
@@ -4,7 +4,7 @@
 {
   environment.systemPackages = with pkgs; [
     steam
-    winetricks
-    wineStaging
+    wineWowPackages.staging
+    (winetricks.override { wine = wineWowPackages.staging; })
   ];
 }
