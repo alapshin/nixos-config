bc809cf6078cda68cd029c74f65635b4c5706753
Andrei Lapshin <alapshin@fastmail.com>
2019-02-17 12:50:34 +0300

    Install skrooge from default channel
    Custom build is no longer needed since relevant bugs where fixed
    upstream.

--- config/overlays/unstable.nix
+++ config/overlays/unstable.nix
@@ -12,14 +12,4 @@ in
 
   jetbrains = unstablePkgs.jetbrains;
   androidStudioPackages = unstablePkgs.androidStudioPackages;
-
-  skrooge = unstablePkgs.skrooge.overrideAttrs (oldAttrs: rec {
-    name = "skrooge-${version}";
-    version = "2.18.0";
-    src = self.fetchgit {
-      url = "https://anongit.kde.org/skrooge.git";
-      rev = "94102bbfff76ee92bd02a45edabcb430f8d8a525";
-      sha256 = "0a1b44jbhgzrhmxcxzk6d4j2hcsqv5kbqbkyrnqgmzm1b6zws43z";
-    };
-  });
 }
