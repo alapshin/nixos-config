ee39e24acb9f2453414cdc9aa574b99bf06b6942
Andrei Lapshin <alapshin@fastmail.com>
2019-11-07 18:01:04 +0300

    Use iwd as wifi backend

--- config/networking.nix
+++ config/networking.nix
@@ -4,10 +4,13 @@
   networking = {
     hostName = "desktop";
     firewall.enable = false;
-    networkmanager.enable = true;
-    networkmanager.packages = with pkgs; [
-      networkmanager-openvpn
-    ];
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+      packages = with pkgs; [
+        networkmanager-openvpn
+      ];
+    };
   };
   boot.extraModulePackages = [ config.boot.kernelPackages.wireguard ];
   environment.systemPackages = [ pkgs.wireguard pkgs.wireguard-tools ];
