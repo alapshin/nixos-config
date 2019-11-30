3961e4621965d0492629aac42c65d9676f85789a
Andrei Lapshin <alapshin@fastmail.com>
2019-11-30 11:28:09 +0300

    Use custom version of nvidia driver
    Current version of nvidia driver from nixpkgs doesn't build with kernel
    5.4

--- config/overlays/custom.nix
+++ config/overlays/custom.nix
@@ -13,4 +13,19 @@ self: super:
 
     patches = [];
   });
+
+  linuxPackages_latest = super.linuxPackages_latest.extend (linuxSelf: linuxSuper:
+  let
+    generic = args: linuxSelf.callPackage (import <nixos/pkgs/os-specific/linux/nvidia-x11/generic.nix> args) { };
+  in
+  {
+    nvidiaPackages = linuxSuper.nvidiaPackages // {
+      beta = generic {
+        version = "440.36";
+        sha256_64bit = "0nbdldwizb802w4x0rqnyb1p7iqz5nqiahqr534n5ihz21a6422h";
+        settingsSha256 = "07hnl3bq76vsl655ipfx9v4zxjq0nc5hp43dk49nny4pi6ly06p1";
+        persistencedSha256 = "08zm1i5sax16xfhkivkmady0yy5argmxv846x21q98ry1ic6cp6w";
+      };
+    };
+  });
 }
