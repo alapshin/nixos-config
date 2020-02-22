5e7cd45435d93e8bfe8a8e75aa478fd2faa8ba9f
Andrei Lapshin <alapshin@fastmail.com>
2020-02-22 10:36:47 +0300

    Cleanup no longer needed package overrides

--- config/overlays/custom.nix
+++ config/overlays/custom.nix
@@ -1,4 +1,4 @@
-# Overlay containing customisations for packages from nixpkgs
+# Overlay containing customizations for packages from nixpkgs
 self: super:
 
 let 
@@ -6,19 +6,12 @@ let
     ref = "master";
     url = https://github.com/NixOS/nixpkgs.git;
   };
-  unstablePkgs = import unstableRepo { config = self.config; overlays = []; };
-
-  mkStudio = args: super.callPackage (import <nixos/pkgs/applications/editors/android-studio/common.nix> args) {
-    fontsConf = super.makeFontsConf {
-      fontDirectories = [];
-    };
-    inherit (super.gnome2) GConf gnome_vfs;
-  };
+  unstablePackages = import unstableRepo { config = self.config; overlays = []; };
 in
 {
-  home-manager = unstablePkgs.home-manager;
+  home-manager = unstablePackages.home-manager;
 
-  jetbrains = unstablePkgs.jetbrains;
+  # jetbrains = unstablePackages.jetbrains;
 
   # skrooge = super.skrooge.overrideAttrs (oldAttrs: rec {
   #   pname = "skrooge";
@@ -31,35 +24,21 @@ in
   #   patches = [];
   # });
 
-  neovim = super.neovim.overrideAttrs (oldAttrs: {
-    preFixup = ''
-        rm -rf $out/share/applications/*.desktop
-    '';
-  });
+  # neovim = unstablePackages.neovim;
+  # androidStudioPackages = unstablePackages.androidStudioPackages;
 
-  androidStudioPackages = unstablePkgs.androidStudioPackages;
-  # androidStudioPackages = super.androidStudioPackages // {
-  #   beta = mkStudio {
-  #     channel = "beta";
-  #     pname = "android-studio-beta";
-  #     version = "3.6.0.18"; # "Android Studio 3.6 RC 1"
-  #     build = "192.6071332";
-  #     sha256Hash = "0xpcihr5xxr9l1kv6aflywshs8fww3s7di0g98mz475whhxwzf3q";
+  # linuxPackages_latest = super.linuxPackages_latest.extend (linuxSelf: linuxSuper:
+  # let
+  #   generic = args: linuxSelf.callPackage (import <nixos/pkgs/os-specific/linux/nvidia-x11/generic.nix> args) { };
+  # in
+  # {
+  #   nvidiaPackages = linuxSuper.nvidiaPackages // {
+  #     beta = generic {
+  #       version = "440.59";
+  #       sha256_64bit = "162gq6w44l8sgnn4qnl2rdlx8c008p04zv4c3i1ps20p21n1mjv1";
+  #       settingsSha256 = "0vxhmirqzyav5ljf0f04yk0az48ir5v0817dq9z9kyqfdvnby93g";
+  #       persistencedSha256 = "0npjh7nashasydp8q6bbcp21w8fc1dycgjy50ics775hjnvm61qn";
+  #     };
   #   };
-  # };
-
-  linuxPackages_latest = super.linuxPackages_latest.extend (linuxSelf: linuxSuper:
-  let
-    generic = args: linuxSelf.callPackage (import <nixos/pkgs/os-specific/linux/nvidia-x11/generic.nix> args) { };
-  in
-  {
-    nvidiaPackages = linuxSuper.nvidiaPackages // {
-      beta = generic {
-        version = "440.59";
-        sha256_64bit = "162gq6w44l8sgnn4qnl2rdlx8c008p04zv4c3i1ps20p21n1mjv1";
-        settingsSha256 = "0vxhmirqzyav5ljf0f04yk0az48ir5v0817dq9z9kyqfdvnby93g";
-        persistencedSha256 = "0npjh7nashasydp8q6bbcp21w8fc1dycgjy50ics775hjnvm61qn";
-      };
-    };
-  });
+  # });
 }
