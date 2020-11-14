5dcd3d2ac06cb23fc1be4670016c160e28fa52c3
Andrei Lapshin <alapshin@fastmail.com>
2020-11-14 15:11:21 +0300

    Remove broken sc-controller package

--- config/gaming.nix
+++ config/gaming.nix
@@ -5,7 +5,6 @@
   programs.steam.enable = true;
   environment.systemPackages = with pkgs; [
     lutris
-    sc-controller
     protontricks
     wineWowPackages.staging
     (winetricks.override { wine = wineWowPackages.staging; })
