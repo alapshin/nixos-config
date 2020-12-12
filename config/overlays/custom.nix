e476e6d81688bc17e7e30c94c8a91bfcac093446
Andrei Lapshin <alapshin@fastmail.com>
2020-12-12 10:19:27 +0300

    Remove no longer needed nvidia overlay

--- config/overlays/custom.nix
+++ config/overlays/custom.nix
@@ -30,19 +30,4 @@ in
   #     inherit (super.gnome2) GConf gnome_vfs;
   #   }; 
   # in packages;
-
-  # linuxPackages_latest = super.linuxPackages_latest.extend (linuxSelf: linuxSuper:
-  # let
-  #   generic = args: linuxSelf.callPackage (import <nixos/pkgs/os-specific/linux/nvidia-x11/generic.nix> args) { };
-  # in
-  # {
-  #   nvidiaPackages = linuxSuper.nvidiaPackages // {
-  #     beta = generic {
-  #       version = "440.82";
-  #       sha256_64bit = "13km9800skyraa0s312fc4hwyw5pzb0jfkrv1yg6anppyan1bm7d";
-  #       settingsSha256 = "15psxvd65wi6hmxmd2vvsp2v0m07axw613hb355nh15r1dpkr3ma";
-  #       persistencedSha256 = "13izz9p2kg9g38gf57g3s2sw7wshp1i9m5pzljh9v82c4c22x1fw";
-  #     };
-  #   };
-  # });
 }
