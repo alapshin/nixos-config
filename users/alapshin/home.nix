7a65fdefc98d4fd6920b7782e981d271a399ef49
Andrei Lapshin <alapshin@fastmail.com>
2021-01-03 15:46:04 +0300

    Use home-manager to setup XDG-enforcing variables

--- users/alapshin/home.nix
+++ users/alapshin/home.nix
@@ -1,38 +1,19 @@
 { config, dirs, myutils, pkgs, ... }:
 let
+  hmcfg = config.home-manager.users."${username}";
   username = myutils.extractUsername (builtins.toString ./.);
 in
 {
   environment = {
-    sessionVariables = {
-      # These are the defaults, and xdg.enable does set them, but due to load
-      # order, they're not set before environment.variables are set, which could
-      # cause race conditions.
-      XDG_BIN_HOME = "$HOME/.local/bin";
-      XDG_CACHE_HOME = "$HOME/.cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME = "$HOME/.local/share";
-    };
-    variables = {
-      ANDROID_SDK_ROOT = "$HOME/opt/android-sdk";
-      # Force some apps to use XDG directories
-      ANDROID_PREFS_ROOT = "$XDG_CONFIG_HOME/android";
-      ANDROID_EMULATOR_HOME = "$XDG_DATA_HOME/android/emulator";
-
-      AWS_CONFIG_FILE = "$XDG_CONFIG_HOME/aws/config";
-      AWS_SHARED_CREDENTIALS_FILE = "$XDG_CONFIG_HOME/aws/credentials";
-      CARGO_HOME = "$XDG_DATA_HOME/cargo";
-      CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
-      DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
-      GNUPGHOME = "$XDG_CONFIG_HOME/gnupg";
-      GRADLE_USER_HOME = "$XDG_DATA_HOME/gradle";
-      IPYTHONDIR = "$XDG_CONFIG_HOME/jupyter";
-      JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
-      MACHINE_STORAGE_PATH = "$XDG_DATA_HOME/docker-machine";
-      ZDOTDIR = "$XDG_CONFIG_HOME/zsh";
-
-      _JAVA_OPTIONS = ''-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'';
-    };
+    # Load home-manager settings
+    # Shells are configured using system module insted of home-manage one and
+    # as a result we need to load home-manager settings by hand
+    loginShellInit = ''
+      hmconfig="/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
+      if [[ -e "$hmconfig" ]]; then
+        source $hmconfig
+      fi
+    '';
   };
 
   home-manager.users."${username}" = {
@@ -73,5 +54,33 @@ in
         "zsh/p10k.zsh".source = "${dirs.dotfiles}/p10k.zsh";
       };
     };
+
+    # Enforce XDG directories usage for some apps
+    home.sessionVariables =
+      let
+        homeDir = hmcfg.home.homeDirectory;
+        xdgDataHome = hmcfg.xdg.dataHome;
+        xdgCacheHome = hmcfg.xdg.cacheHome;
+        xdgConfigHome = hmcfg.xdg.configHome;
+      in
+      {
+        ANDROID_SDK_ROOT = "${homeDir}/opt/android-sdk";
+        ANDROID_PREFS_ROOT = "${xdgConfigHome}/android";
+        ANDROID_EMULATOR_HOME = "${xdgDataHome}/android/emulator";
+
+        AWS_CONFIG_FILE = "${xdgConfigHome}/aws/config";
+        AWS_SHARED_CREDENTIALS_FILE = "${xdgConfigHome}/aws/credentials";
+        CARGO_HOME = "${xdgDataHome}/cargo";
+        CUDA_CACHE_PATH = "${xdgCacheHome}/nv";
+        DOCKER_CONFIG = "${xdgConfigHome}/docker";
+        GNUPGHOME = "${xdgConfigHome}/gnupg";
+        GRADLE_USER_HOME = "${xdgDataHome}/gradle";
+        IPYTHONDIR = "${xdgConfigHome}/jupyter";
+        JUPYTER_CONFIG_DIR = "${xdgConfigHome}/jupyter";
+        MACHINE_STORAGE_PATH = "${xdgDataHome}/docker-machine";
+        ZDOTDIR = "${xdgConfigHome}/zsh";
+
+        _JAVA_OPTIONS = ''-Djava.util.prefs.userRoot=${xdgConfigHome}/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'';
+      };
   };
 }
