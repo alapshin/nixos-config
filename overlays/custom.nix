b45195903985c879f502b2f3ac2d3e244dd40ad5
Andrei Lapshin <alapshin@fastmail.com>
2021-01-04 17:16:00 +0300

    Remove no longer needed package overrides

--- overlays/custom.nix
+++ overlays/custom.nix
@@ -1,15 +1,4 @@
 final: prev:
 {
-  mesa = (prev.mesa.override {
-    llvmPackages = prev.llvmPackages_latest;
-  }).overrideAttrs (oldAttrs: rec {
-    version = "20.3.1";
-    src = prev.fetchurl {
-      urls = [
-        "https://mesa.freedesktop.org/archive/mesa-${version}.tar.xz"
-      ];
-      sha256 = "03vqm9kqrcpijg6bxldj0bg360z8d7c767n3b16jdc1apd4inxdg";
-    };
-  });
-  llvmPackages = prev.llvmPackages_latest;
+  # llvmPackages = prev.llvmPackages_latest;
 }
