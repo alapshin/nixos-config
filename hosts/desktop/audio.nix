1afd3605fbc617b3d39f5bb22665c0a8ed590d32
Andrei Lapshin <alapshin@fastmail.com>
2021-05-29 15:17:35 +0300

    Enable PipeWire

--- hosts/desktop/audio.nix
+++ hosts/desktop/audio.nix
@@ -1,35 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  hardware = {
-    pulseaudio = {
+  services.pipewire = {
+    enable = true;
+    alsa = {
       enable = true;
-      package = pkgs.pulseaudioFull;
       support32Bit = true;
     };
-  };
-
-  services.jack = {
-    # support ALSA only programs via ALSA JACK PCM plugin
-    alsa = {
-      enable = false;
-    };
-    # support ALSA only programs via loopback device (supports programs like Steam)
-    loopback = {
-      enable = false;
-    };
-    jackd = {
-      enable = false;
-      # extraOptions = [
-      #   "-v" "-dalsa" "-r48000" "-p1024" "-n2" "-D" "-Chw:Microphone" "-Phw:ODACrevB"
-      # ];
-    };
+    pulse.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
     kwave
-    jack2
-    qjackctl
     obs-studio
     pavucontrol
   ];
