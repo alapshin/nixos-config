0e8b549891400f606a70249cc042d62a9f7bf3c6
Andrei Lapshin <alapshin@fastmail.com>
2021-07-05 12:58:28 +0300

    Update Android Studio

--- packages/android-studio/default.nix
+++ packages/android-studio/default.nix
@@ -9,17 +9,17 @@ let
     inherit buildFHSUserEnv;
   };
   stableVersion = {
-    version = "4.2.1.0"; # "Android Studio 4.2.1"
-    build = "202.7351085";
-    sha256Hash = "074y6i0h8zamjgvvs882im44clds3g6aq8rssl7sq1wx6hrn5q36";
+    version = "4.2.2.0"; # "Android Studio 4.2.2"
+    build = "202.7486908";
+    sha256Hash = "18zc9xr2xmphj6m6a1ilwripmvqzplp2583afq1pzzz3cv5h8fvk";
   };
   betaVersion = {
-    version = "2020.3.1.18"; # "Android Studio Arctic Fox (2020.3.1) Beta 3"
-    sha256Hash = "16hg21xjjqyjii7ngq0ddqhjwrk0lr6vkn2i4rrxqwfnihdmbzp4";
+    version = "2020.3.1.20"; # "Android Studio Arctic Fox (2020.3.1) Beta 5"
+    sha256Hash = "0swcsjx29ar4b0c8yhbynshqdn2sv94ga58h2nrc99927vp17g85";
   };
   latestVersion = { # canary & dev
-    version = "2021.1.1.1"; # "Android Studio Bumblebee (2021.1.1) Canary 1"
-    sha256Hash = "0aavmk8byw817356jm28rl998gcp3zm7x3fq14hm2awzhk5jaklm";
+    version = "2021.1.1.2"; # "Android Studio Bumblebee (2021.1.1) Canary 2"
+    sha256Hash = "10cp4sx6k6hnh5z7r1hf957h7zsq8xncjz83s793ibnki4s1qadr";
   };
 in {
   # Attributes are named by their corresponding release channels
