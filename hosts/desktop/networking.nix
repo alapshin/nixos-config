b9b65f04a39bc34be7f3b5814371508a4e83ff6f
Andrei Lapshin <alapshin@fastmail.com>
2022-04-15 18:14:49 +0300

    Enable wake-on-lan

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -4,5 +4,10 @@
   networking = {
     hostName = "desktop";
     networkmanager.wifi.backend = "iwd";
+    interfaces = {
+      "enp42s0" = {
+        wakeOnLan.enable = true;
+      };
+    };
   };
 }
