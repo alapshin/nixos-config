df236c36de00e90ca06a413a9438b4c0622d8702
Andrei Lapshin <alapshin@fastmail.com>
2022-06-27 10:49:41 +0300

    Install additional locales

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -41,6 +41,11 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_DK.UTF-8/UTF-8"
+      "ru_RU.UTF-8/UTF-8"
+    ];
   };
 
   networking = {
