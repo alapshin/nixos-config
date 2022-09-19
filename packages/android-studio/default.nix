8ebc34044afcb078120e0d6371e5a357d46e48dd
Andrei Lapshin <alapshin@fastmail.com>
2022-09-19 18:58:41 +0300

    Update Android Studio

--- packages/android-studio/default.nix
+++ packages/android-studio/default.nix
@@ -10,16 +10,16 @@ let
     inherit tiling_wm;
   };
   stableVersion = {
-    version = "2021.2.1.15"; # "Android Studio Chipmunk (2021.2.1)"
-    sha256Hash = "ABjg38DdKSFwBRb3osRDN3xVd4jaf7CkUkPstDAHRb4=";
+    version = "2021.3.1.16"; # "Android Studio Dolphin (2021.3.1)"
+    sha256Hash = "16hxz47fa8gigw2k9qp6npsf6ih46q1zgjin8fa3px46axc5nwhs";
   };
   betaVersion = {
     version = "2021.3.1.14"; # "Android Studio Dolphin (2021.3.1) Beta 5"
     sha256Hash = "k1Qt54u45rwHsQNz9TVqnFB65kBKtfFZ3OknpfutKPI=";
   };
   latestVersion = { # canary & dev
-    version = "2022.1.1.8"; # "Android Studio Electric Eel (2022.1.1) Canary 8"
-    sha256Hash = "0bZXx4YpMztLAnwuBaSaNT3GJNfYnqCDanwR+Q7qyUc=";
+    version = "2022.1.1.10"; # "Android Studio Electric Eel (2022.1.1) Canary 10"
+    sha256Hash = "0jp0ag23v0dwnkbk0pyfs0rsdzqr802sz0x586zm6nxz23j9h5p8";
   };
 in {
   # Attributes are named by their corresponding release channels
