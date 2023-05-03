b41cad5163f411b635ccf155cb14e55fb4a30613
Andrei Lapshin <alapshin@fastmail.com>
2023-05-03 19:35:11 +0200

    Update nix settings

--- configuration.nix
+++ configuration.nix
@@ -13,16 +13,18 @@
       options = "--delete-older-than 7d";
     };
     package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-    nixPath = [
-      "nixpkgs=${inputs.nixos}"
-    ];
-    registry = {
-      nixos.flake = inputs.nixos;
-      nixpkgs.flake = inputs.nixpkgs;
+    settings = {
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
     };
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
   };
 
   nixpkgs = {
