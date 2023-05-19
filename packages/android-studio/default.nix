13ab9736ddf524237a6197d16544465e6b1750fb
Andrei Lapshin <alapshin@fastmail.com>
2023-05-19 14:35:35 +0200

    Update Android Studio

--- packages/android-studio/default.nix
+++ packages/android-studio/default.nix
@@ -10,16 +10,16 @@ let
     inherit tiling_wm;
   };
   stableVersion = {
-    version = "2022.2.1.18"; # "Android Studio Flamingo (2022.2.1)"
-    sha256Hash = "sha256-zdhSxEmbX3QC30Tfxp6MpBj/yaaEyqs0BHR2/SyyTvw=";
+    version = "2022.2.1.19"; # "Android Studio Flamingo (2022.2.1) Patch 1"
+    sha256Hash = "sha256-bAtPlJI3RwqQX6xpEi7S8T2IDc/39MONU3iFpfi8v3A=";
   };
   betaVersion = {
-    version = "2022.3.1.12"; # "Android Studio Giraffe (2022.3.1) Beta 1"
-    sha256Hash = "sha256-Wy5iifscL1Ko7ZInx/uAvyJyM4cS6LfTYWxdJbZk6po=";
+    version = "2022.3.1.14"; # "Android Studio Giraffe (2022.3.1) Beta 3"
+    sha256Hash = "sha256-hm1iQLrzAnzRR43/+T8wyPyOCDjwY2Puwmd/z4khOIE=";
   };
   latestVersion = {
-    version = "2023.1.1.1"; # "Android Studio Hedgehog (2023.1.1) Canary 1"
-    sha256Hash = "sha256-ZiTTAYU/Lj87U/RkrX7ThR31StaR3T15vWvqRn+wDFs=";
+    version = "2023.1.1.4"; # "Android Studio Hedgehog (2023.1.1) Canary 4"
+    sha256Hash = "sha256-L0Plmv3Y75QBLCitpxCwLEEJ+4s+Pz0g38tQpoAK6h4=";
   };
 in {
   # Attributes are named by their corresponding release channels
