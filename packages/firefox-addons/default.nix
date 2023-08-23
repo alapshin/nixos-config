a15c7eb0f0156293e297b143218e56d4788431f1
Andrei Lapshin <alapshin@fastmail.com>
2023-08-23 19:16:47 +0200

    Update bypass-paywalls-clean

--- packages/firefox-addons/default.nix
+++ packages/firefox-addons/default.nix
@@ -38,14 +38,14 @@ in
 
   bypass-paywalls-clean =
     let
-      version = "3.2.8.0";
+      version = "3.3.0.0";
     in
     buildFirefoxXpiAddon {
       pname = "bypass-paywalls-clean";
       inherit version;
       addonId = "{d133e097-46d9-4ecc-9903-fa6a722a6e0e}";
       url = "https://gitlab.com/magnolia1234/bpc-uploads/-/raw/master/bypass_paywalls_clean-${version}.xpi";
-      sha256 = "sha256-BZ6eu0mjeUHSuj/ikLf8ZH62bfDC1Aqa8CR1vJNN9Js=";
+      sha256 = "sha256-0dzVa+RaR9Nzp81T0sI0Cs/A3kq+Km+3nFF2JIN4Fv0=";
       meta = with lib; {
         homepage = "https://gitlab.com/magnolia1234/bypass-paywalls-firefox-clean";
         description = "Bypass Paywalls of (custom) news sites";
