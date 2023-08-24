29a1e08ae0a001f7be45d64a34298934227774e8
Andrei Lapshin <alapshin@fastmail.com>
2023-08-24 22:12:11 +0200

    Enable auto-cpufreq

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -3,4 +3,17 @@
 , ...
 }: {
   services.fwupd.enable = true;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        turbo = "always";
+        governor = "performance";
+      };
+      battery = {
+        turbo = "never";
+        governor = "powersave";
+      };
+    };
+  };
 }
