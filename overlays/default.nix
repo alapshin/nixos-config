49e12ef5f61fade62b500540210db0581ffadc9b
Andrei Lapshin <alapshin@fastmail.com>
2023-09-15 08:50:22 +0200

    Add workaround for using bismuth with KDE 5.27

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,6 +22,30 @@
         "JetBrainsMono"
       ];
     };
+
+    libsForQt5 = prev.libsForQt5 // {
+      bismuth = prev.libsForQt5.bismuth.overrideAttrs(finalAttrs: previousAttrs: {
+        patches =
+          (previousAttrs.patches or [])
+          ++ [
+            # (fetchpatch {
+            #   name = "bismuth-3.1-4-border-color.patch";
+            #   url = "https://github.com/I-Want-ToBelieve/bismuth/commit/dac110934fe1ae0da9e4aca8c331f27987b033cf.patch";
+            #   sha256 = "sha256-3fQs/A4hc/qeiu+792nZBTl4ujg8rQD25kuwNr03YUs=";
+            # })
+            (prev.fetchpatch {
+              name = "bismuth-3.1-4-static-block.patch";
+              url = "https://github.com/I-Want-ToBelieve/bismuth/commit/99438b55a82f90d4df3653d00f1f0978eddc2725.patch";
+              sha256 = "sha256-jEt0YdS7k0bJRIS0UMY21o71jgrJcwNp3gFA8e8TG6I=";
+            })
+            (prev.fetchpatch {
+              name = "bismuth-3.1-4-window-id.patch";
+              url = "https://github.com/jkcdarunday/bismuth/commit/ce377a33232b7eac80e7d99cb795962a057643ae.patch";
+              sha256 = "sha256-15txf7pRhIvqsrBdBQOH1JDQGim2Kh5kifxQzVs5Zm0=";
+            })
+          ];
+        });
+      };
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
