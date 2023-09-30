5333dc8cdba7ba9fa2d0a0f0bb0502b4df9ee6b7
Andrei Lapshin <alapshin@fastmail.com>
2023-09-30 18:54:56 +0200

    Install yt-dlp on media server

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -17,4 +17,8 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlJo3xdypmwSS2lsHCzf6GsqyEGvr+HzvbU+TGuPjmA"
   ];
+
+  environment.systemPackages = with pkgs; [
+    yt-dlp
+  ];
 }
