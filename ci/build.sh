3456be122a89c9c91934da110e0e83b049bb42eb
Andrei Lapshin <alapshin@fastmail.com>
2023-11-24 08:53:55 +0100

    Update build script

--- ci/build.sh
+++ ci/build.sh
@@ -8,42 +8,49 @@ secret_files=(
     "secrets/accounts.json"
 )
 
-function reset() {
+function reset {
     for f in "${secret_files[@]}"; do
         git restore "${f}"
     done;
 
 }
 
-function encrypt() {
+function encrypt {
     for f in "${secret_files[@]}"; do
         sops --encrypt --in-place "${f}"
     done;
 }
 
-function decrypt() {
+function decrypt {
     for f in "${secret_files[@]}"; do
         sops --decrypt --in-place "${f}"
     done;
 }
 
-function check() {
+function check {
+    decrypt
     nix flake check
 }
 
-function switch() {
-    nixos-rebuild "${command}" --use-remote-sudo --flake "${flake_uri}" 
+function update {
+    nix flake update
+}
+
+function rebuild {
+    decrypt
+    sudo nixos-rebuild switch --flake "${flake_uri}"
 }
 
 trap reset EXIT
-
-decrypt
 
 case $command in
     check)
         check
         ;;
-    switch)
-        switch
+    rebuild)
+        rebuild
+        ;;
+    update)
+        update
         ;;
 esac
