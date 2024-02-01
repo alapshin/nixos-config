e3a71c9b8fb297cb3c35528559f2a3c0ea39d315
Andrei Lapshin <alapshin@fastmail.com>
2024-02-01 16:45:55 +0100

    Setup catppuccin theme for zsh-syntax-highlighting

--- users/alapshin/home/shell.nix
+++ users/alapshin/home/shell.nix
@@ -124,6 +124,16 @@
           src = pkgs.zsh-powerlevel10k;
           file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
         }
+        {
+          name = "zsh-syntax-highlighting-catpuccin";
+          file = "themes/catppuccin_latte-zsh-syntax-highlighting.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "zsh-syntax-highlighting";
+            rev = "06d519c20798f0ebe275fc3a8101841faaeee8ea";
+            sha256 = "sha256-Q7KmwUd9fblprL55W0Sf4g7lRcemnhjh4/v+TacJSfo=";
+          };
+        }
       ];
 
       oh-my-zsh = {
