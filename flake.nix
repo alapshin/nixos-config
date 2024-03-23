7d72ce3fa685dbc72637792b1faf2cf75ce27ecb
Andrei Lapshin <alapshin@fastmail.com>
2024-03-23 20:09:25 +0100

    Update disko version

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     nur.url = "github:nix-community/nur";
 
-    disko.url = "github:nix-community/disko?ref=v1.3.0";
+    disko.url = "github:nix-community/disko?ref=v1.4.1";
     disko.inputs.nixpkgs.follows = "nixos";
 
     sops-nix.url = "github:Mic92/sops-nix";
