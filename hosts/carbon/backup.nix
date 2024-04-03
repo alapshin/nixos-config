6be82c75226b570c4aa2a165bc30549acdd5535a
Andrei Lapshin <alapshin@fastmail.com>
2024-04-03 22:03:43 +0200

    Setup borgbackup to storagebox

--- hosts/carbon/backup.nix
+++ hosts/carbon/backup.nix
@@ -1,15 +1,36 @@
-{ config
-, pkgs
-, ...
-}: {
+{ config, ... }:
+let
+  storagebox = "u399502.your-storagebox.de";
+in
+{
+
+  sops.secrets = {
+    "borg/keyfile" = {
+      format = "binary";
+      sopsFile = ./secrets/borg/keyfile;
+    };
+    "borg/passphrase" = {
+      sopsFile = ./secrets/borg/passphrase.yaml;
+    };
+    "borg/borg_ed25519" = {
+      mode = "0600";
+      format = "binary";
+      sopsFile = ./secrets/borg/borg_ed25519;
+    };
+    "borg/borg_ed25519.pub" = {
+      mode = "0600";
+      format = "binary";
+      sopsFile = ./secrets/borg/borg_ed25519.pub;
+    };
+  };
   services.borgbackup.jobs = {
-    "carbon" = {
+    "home" = {
       paths = [
         "/home/alapshin/calibre"
         "/home/alapshin/Documents"
         "/home/alapshin/Syncthing"
       ];
-      repo = "borg@alapshin.com:.";
+      repo = "ssh://u399502@${storagebox}:23/./borgbackup/carbon";
       encryption = {
         mode = "repokey-blake2";
         passCommand = "cat ${config.sops.secrets."borg/passphrase".path}";
@@ -32,5 +53,11 @@
     };
   };
 
-  systemd.timers."borgbackup-job-carbon".wants = [ "network-online.target" ];
+  systemd.timers."borgbackup-job-home".wants = [ "network-online.target" ];
+
+  programs.ssh.knownHosts = {
+    "${storagebox}" = {
+      publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5EB5p/5Hp3hGW1oHok+PIOH9Pbn7cnUiGmUEBrCVjnAw+HrKyN8bYVV0dIGllswYXwkG/+bgiBlE6IVIBAq+JwVWu1Sss3KarHY3OvFJUXZoZyRRg/Gc/+LRCE7lyKpwWQ70dbelGRyyJFH36eNv6ySXoUYtGkwlU5IVaHPApOxe4LHPZa/qhSRbPo2hwoh0orCtgejRebNtW5nlx00DNFgsvn8Svz2cIYLxsPVzKgUxs8Zxsxgn+Q/UvR7uq4AbAhyBMLxv7DjJ1pc7PJocuTno2Rw9uMZi1gkjbnmiOh6TTXIEWbnroyIhwc8555uto9melEUmWNQ+C+PwAK+MPw==";
+    };
+  };
 }
--- hosts/carbon/secrets.nix
+++ hosts/carbon/secrets.nix
@@ -4,21 +4,6 @@
 }: {
   sops = {
     secrets = {
-      "borg/passphrase" = {
-        sopsFile = ./secrets/borg/passphrase.yaml;
-      };
-      "borg/keyfile" = {
-        format = "binary";
-        sopsFile = ./secrets/borg/keyfile;
-      };
-      "borg/borg_ed25519" = {
-        format = "binary";
-        sopsFile = ./secrets/borg/borg_ed25519;
-      };
-      "borg/borg_ed25519.pub" = {
-        format = "binary";
-        sopsFile = ./secrets/borg/borg_ed25519.pub;
-      };
       "wireguard/public_key" = {
         format = "binary";
         sopsFile = ./secrets/wireguard/public_key;
