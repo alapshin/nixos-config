35a1b2c149fdc86239cffee08456216b60cdf612
Andrei Lapshin <alapshin@fastmail.com>
2024-04-14 17:18:34 +0200

    Update GnuPG settings

--- users/alapshin/home/gnupg.nix
+++ users/alapshin/home/gnupg.nix
@@ -5,9 +5,17 @@
   programs.gpg = {
     enable = true;
     homedir = "${config.xdg.dataHome}/gnupg";
+    settings = {
+        keyid-format = "long";
+        with-keygrip = true;
+        with-key-origin = true;
+        with-fingerprint = true;
+        with-subkey-fingerprint = true;
+    };
   };
   services.gpg-agent = {
     enable = true;
     enableZshIntegration = true;
+    pinentryPackage = pkgs.pinentry-qt;
   };
 }
