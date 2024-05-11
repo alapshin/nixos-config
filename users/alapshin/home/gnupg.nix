36119bb4c17bbee125dbe9fa11cbee388b2821d0
Andrei Lapshin <alapshin@fastmail.com>
2024-05-11 22:48:19 +0200

    Reformat

--- users/alapshin/home/gnupg.nix
+++ users/alapshin/home/gnupg.nix
@@ -6,11 +6,11 @@
     enable = true;
     homedir = "${config.xdg.dataHome}/gnupg";
     settings = {
-        keyid-format = "long";
-        with-keygrip = true;
-        with-key-origin = true;
-        with-fingerprint = true;
-        with-subkey-fingerprint = true;
+      keyid-format = "long";
+      with-keygrip = true;
+      with-key-origin = true;
+      with-fingerprint = true;
+      with-subkey-fingerprint = true;
     };
   };
   services.gpg-agent = {
--- users/alapshin/home/ssh.nix
+++ users/alapshin/home/ssh.nix
@@ -10,20 +10,20 @@
   };
 
   programs = {
-      ssh = {
-          enable = true;
-          addKeysToAgent = "yes";
-          matchBlocks = {
-              "gelmir" = {
-                  user = "root";
-                  port = 2222;
-                  hostname = "37.27.114.205";
-              };
-              "niflheim" = {
-                  user= "root";
-                  hostname = "37.27.114.205";
-              };
-          };
+    ssh = {
+      enable = true;
+      addKeysToAgent = "yes";
+      matchBlocks = {
+        "gelmir" = {
+          user = "root";
+          port = 2222;
+          hostname = "37.27.114.205";
+        };
+        "niflheim" = {
+          user = "root";
+          hostname = "37.27.114.205";
+        };
       };
+    };
   };
 }
