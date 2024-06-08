42e5b72ef3c9484ddc812d9124e0bcc32072d09c
Andrei Lapshin <alapshin@fastmail.com>
2024-06-08 21:27:47 +0200

    Remove old server configs

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -4,23 +4,9 @@
 , ...
 }: {
   imports = [
-    ./acme.nix
-    ./audiobookshelf.nix
-    ./authelia.nix
-    ./calibre.nix
-    ./dashboard.nix
-    ./lldap.nix
-    ./media.nix
-    ./nginx.nix
-    ./nextcloud.nix
     ./openssh.nix
-    ./postgres.nix
     ./secrets.nix
-    ./syncthing.nix
-    ./wireguard.nix
     ./xray-server.nix
-
-    ./pg-upgrade.nix
 
     ./networking.nix
     ./hardware-configuration.nix
@@ -29,9 +15,5 @@
   users.users.root.hashedPasswordFile = config.sops.secrets."linux/root".path;
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlJo3xdypmwSS2lsHCzf6GsqyEGvr+HzvbU+TGuPjmA"
-  ];
-
-  environment.systemPackages = with pkgs; [
-    yt-dlp
   ];
 }
