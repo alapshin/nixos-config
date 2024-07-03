14bfb205843e34b9e89c8979db058eca238b6ff6
Andrei Lapshin <alapshin@fastmail.com>
2024-07-04 00:04:16 +0200

    Fix IPv6 network setup

--- hosts/niflheim/networking.nix
+++ hosts/niflheim/networking.nix
@@ -14,6 +14,17 @@
 
   systemd.network = {
     enable = true;
+
+    # See https://wiki.nixos.org/wiki/Install_NixOS_on_Hetzner_Cloud#Network_configuration
+    networks."10-wan" = {
+      # Either ens3 (amd64) or enp1s0 (arm64)
+      matchConfig.Name = "enp6s0";
+      networkConfig = {
+        DHCP = "ipv4";
+        Gateway = "fe80::1";
+        Address = "2a01:4f9:3070:2556::/64";
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
