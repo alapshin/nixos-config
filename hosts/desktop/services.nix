0b82c0efe7879f5f4be780be31cb59a57fa09f31
Andrei Lapshin <alapshin@fastmail.com>
2024-07-30 16:40:41 +0200

    Update desktop timezone

--- hosts/desktop/services.nix
+++ hosts/desktop/services.nix
@@ -1,9 +1,19 @@
-{ config
+{ lib
 , pkgs
+, config
 , ...
 }: {
   services = {
+    avahi = {
+      enable = true;
+    };
     openssh = {
+      enable = true;
+    };
+    geoclue2 = {
+      enable = true;
+    };
+    automatic-timezoned = {
       enable = true;
     };
   };
