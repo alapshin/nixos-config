41c39a4ff91674a4c9449f96cc8935f5a0ebc080
Andrei Lapshin <alapshin@fastmail.com>
2024-07-30 14:47:12 +0300

    Remove LUKS USB drive

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -43,13 +43,11 @@
         "luksroot" = {
           device = "/dev/disk/by-uuid/56cd0776-0e1a-46b2-8e61-56a1aab91c4a";
           keyFileSize = 4096;
-          keyFile = "/dev/disk/by-uuid/2022-08-09-12-03-48-00";
           allowDiscards = true;
         };
         "luksdata" = {
           device = "/dev/disk/by-uuid/f8c9d40f-d397-46d2-a058-55a225d2670e";
           keyFileSize = 4096;
-          keyFile = "/dev/disk/by-uuid/2022-08-09-12-03-48-00";
           allowDiscards = true;
         };
       };
@@ -62,14 +60,14 @@
       fsType = "btrfs";
       options = [ "subvol=root" "discard=async" ];
     };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/A0D1-44CF";
+      fsType = "vfat";
+    };
     "/home" = {
       device = "/dev/disk/by-uuid/20b4e7b3-3a81-468e-9ca9-2fdc1b6c2238";
       fsType = "btrfs";
       options = [ "subvol=home" "discard=async" ];
-    };
-    "/boot" = {
-      device = "/dev/disk/by-uuid/A0D1-44CF";
-      fsType = "vfat";
     };
     "/mnt/data" = {
       device = "/dev/disk/by-uuid/1a34979e-9d0a-47bf-a2a8-2034afddec19";
