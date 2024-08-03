c82937174465483b4ab6aa9cb75f58a4db6f23c1
Andrei Lapshin <alapshin@fastmail.com>
2024-08-03 19:09:01 +0200

    Remove no longer used secrets

--- hosts/common/secrets.nix
+++ hosts/common/secrets.nix
@@ -1,5 +1,6 @@
-{ config
+{ lib
 , pkgs
+, config
 , ...
 }: {
   sops = {
@@ -9,22 +10,5 @@
       ];
     };
     defaultSopsFile = ./secrets.yaml;
-    # secrets = {
-    #   "borg/encryption_passphrase" = {
-    #     sopsFile = ../../secrets/borg/passphrase.yaml;
-    #   };
-    #   "borg/borgbase_ed25519" = {
-    #     format = "binary";
-    #     sopsFile = ../../secrets/borg/borgbase_ed25519;
-    #   };
-    #   "borg/borgbase_ed25519.pub" = {
-    #     format = "binary";
-    #     sopsFile = ../../secrets/borg/borgbase_ed25519.pub;
-    #   };
-    #   "borg/keys/rm4i22x5_repo_borgbase_com__repo" = {
-    #     format = "binary";
-    #     sopsFile = ../../secrets/borg/keys/rm4i22x5_repo_borgbase_com__repo;
-    #   };
-    # };
   };
 }
