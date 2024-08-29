58aa0b0bd5f180d01a7379d886d51f812926f945
Andrei Lapshin <alapshin@fastmail.com>
2024-08-29 17:28:15 +0200

    Setup SSO for PhotoPrism

--- hosts/niflheim/photoprism.nix
+++ hosts/niflheim/photoprism.nix
@@ -9,19 +9,58 @@
     secrets = {
       "photoprism/admin_username" = { };
       "photoprism/admin_password" = { };
+      "photoprism/oidc_client_secret" = { };
     };
+    templates."photoprism.env".content = ''
+      PHOTOPRISM_OIDC_SECRET=${config.sops.placeholder."photoprism/oidc_client_secret"}
+    '';
   };
+
   services = {
     photoprism = {
       enable = true;
       originalsPath = "/mnt/data/photos";
       passwordFile = config.sops.secrets."photoprism/admin_password".path;
+      settings = {
+        PHOTOPRISM_OIDC_WEBDAV = "true";
+        PHOTOPRISM_OIDC_REGISTER = "true";
+        PHOTOPRISM_OIDC_URI = "https://${config.domain.auth}";
+        PHOTOPRISM_OIDC_CLIENT = "photoprism";
+        PHOTOPRISM_OIDC_PROVIDER = "Authelia";
+
+        PHOTOPRISM_SITE_URL = "https://photoprism.${config.domain.base}";
+      };
     };
 
     nginx-ext.applications."photoprism" = {
       auth = false;
       port = config.services.photoprism.port;
       proxyWebsockets = true;
+    };
+
+    authelia.instances."main".settings = {
+      identity_providers = {
+        oidc = {
+          clients = [
+            {
+              client_id = "photoprism";
+              client_name = "PhotoPrism";
+              client_secret = "$pbkdf2-sha512$310000$p3cinO7wg61tGTLjc9u4Fw$sMP0JHSEPyQjQeDzCPJamQ08fz.ulv7neS3nwU2ZM8zMNMSfn3EI7l1RIOrkUZe.bT.RKXcQz/SjjpwtUnNMMg";
+              authorization_policy = "one_factor";
+              scopes = [
+                "address"
+                "email"
+                "groups"
+                "openid"
+                "profile"
+              ];
+              redirect_uris = [
+                "https://photoprism.${config.domain.base}/api/v1/oidc/redirect"
+              ];
+            }
+          ];
+        };
+      };
     };
   };
 
@@ -34,4 +73,9 @@
       };
     };
   };
+
+  systemd.services.photoprism.serviceConfig = {
+    EnvironmentFile = config.sops.templates."photoprism.env".path;
+  };
+
 }
