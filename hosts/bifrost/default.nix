4dc432573d627d5bfb2dda328a8b8483cb1a4512
Andrei Lapshin <alapshin@fastmail.com>
2024-10-08 15:07:30 +0200

    Add ssh key

--- hosts/bifrost/default.nix
+++ hosts/bifrost/default.nix
@@ -14,8 +14,13 @@
     ./hardware-configuration.nix
   ];
 
-  users.users.root.hashedPasswordFile = config.sops.secrets."linux/root".path;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlJo3xdypmwSS2lsHCzf6GsqyEGvr+HzvbU+TGuPjmA"
-  ];
+  users.users.root = {
+    hashedPasswordFile = config.sops.secrets."linux/root".path;
+    openssh.authorizedKeys.keys = [
+      # desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFDz59Mf8DGoOjluY9T4FNFaOvXH1s/VVZ9awHcyNVHJ"
+      # laptop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlJo3xdypmwSS2lsHCzf6GsqyEGvr+HzvbU+TGuPjmA"
+    ];
+  };
 }
