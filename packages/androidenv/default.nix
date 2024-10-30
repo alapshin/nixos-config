8a3509cf9b6d7e7682c909af17645fab773676a9
Andrei Lapshin <alapshin@fastmail.com>
2024-10-30 21:11:12 +0100

    Update android env

--- packages/androidenv/default.nix
+++ packages/androidenv/default.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 
-let
-  buildToolsVersion = "35.0.0";
-in
 pkgs.androidenv.composeAndroidPackages {
   repoJson = ./repo.json;
 
   toolsVersion = "26.1.1";
   cmdLineToolsVersion = "16.0";
   platformToolsVersion = "35.0.2";
-  buildToolsVersions = [ buildToolsVersion ];
+  buildToolsVersions = [
+    "34.0.0"
+    "35.0.0"
+  ];
 
   includeNDK = true;
   ndkVersions = [ "27.2.12479018" ];
@@ -20,6 +20,6 @@ pkgs.androidenv.composeAndroidPackages {
   includeSources = true;
   includeSystemImages = true;
   abiVersions = [ "x86_64" "x86" ];
-  platformVersions = [ "35" ];
+  platformVersions = [ "34" "35" ];
   systemImageTypes = [ "google_apis_playstore" ];
 }
