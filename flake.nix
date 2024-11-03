c9e2ada297018987dd25bf58c51c2065a841c39a
Andrei Lapshin <alapshin@fastmail.com>
2024-11-03 17:00:53 +0100

    Update lock file

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
 
     nur.url = "github:nix-community/nur";
 
-    disko.url = "github:nix-community/disko?ref=v1.8.2";
+    disko.url = "github:nix-community/disko?ref=v1.9.0";
     disko.inputs.nixpkgs.follows = "nixos";
 
     sops-nix.url = "github:Mic92/sops-nix";
