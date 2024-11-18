92b0f148dfa69e92fe7e0f1cd50572fb3dd4deae
Andrei Lapshin <alapshin@fastmail.com>
2024-11-18 22:30:22 +0100

    Fix changedetection 400 error

--- hosts/niflheim/changedetection.nix
+++ hosts/niflheim/changedetection.nix
@@ -10,6 +10,16 @@
       enable = true;
       behindProxy = true;
     };
+    # Workaround for https://github.com/dgtlmoon/changedetection.io/discussions/2259
+    nginx.virtualHosts."cdio.${config.domain.base}" = {
+      locations = {
+        "/" = {
+          extraConfig = ''
+            proxy_set_header Referer "https://cdio.${config.domain.base}";
+          '';
+        };
+      };
+    };
     nginx-ext.applications."cdio" = {
       auth = true;
       port = config.services.changedetection-io.port;
