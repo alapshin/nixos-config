b386e418ee26228963d7837d8399d1d9e21ef46b
Andrei Lapshin <alapshin@fastmail.com>
2024-12-29 16:46:13 +0100

    Enable substituters for remote machines

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
       # Nix community's cache server public key
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    builders-use-substitutes = true;
   };
 
   inputs = {
