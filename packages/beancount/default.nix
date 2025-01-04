e6e098b9f0277dccf9298b6080e0c6bd0b2741a1
Andrei Lapshin <alapshin@fastmail.com>
2025-01-04 10:29:14 +0100

    Update beancount package

--- packages/beancount/default.nix
+++ packages/beancount/default.nix
@@ -1,26 +1,21 @@
 {
   lib,
+  beautifulsoup4,
   buildPythonPackage,
+  click,
   fetchPypi,
   isPy3k,
-  beautifulsoup4,
-  bottle,
-  chardet,
+  meson,
+  meson-python,
+  pytestCheckHook,
   python-dateutil,
-  google-api-python-client,
-  google-auth-oauthlib,
-  lxml,
-  oauth2client,
-  ply,
-  pytest,
-  python-magic,
-  requests,
+  regex,
 }:
 
 buildPythonPackage rec {
   version = "3.0.0";
-  format = "setuptools";
   pname = "beancount";
+  pyproject = true;
 
   disabled = !isPy3k;
 
@@ -30,24 +25,32 @@ buildPythonPackage rec {
   };
 
   # Tests require files not included in the PyPI archive.
+  # Also there is an import error after migration to meson build.
   doCheck = false;
+
+  build-system = [
+    meson
+    meson-python
+  ];
+
+  depedencies = [
+    click
+    python-dateutil
+    regex
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 
   propagatedBuildInputs = [
     beautifulsoup4
-    bottle
-    chardet
+    click
     python-dateutil
-    google-api-python-client
-    google-auth-oauthlib
-    lxml
-    oauth2client
-    ply
-    python-magic
-    requests
-    # pytest really is a runtime dependency
-    # https://github.com/beancount/beancount/blob/v2/setup.py#L81-L82
-    pytest
+    regex
   ];
+
+  pythonImportsCheck = [ "beancount" ];
 
   meta = with lib; {
     homepage = "https://github.com/beancount/beancount";
