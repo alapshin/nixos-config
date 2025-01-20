4e85654f0048f106b09cd34ef43b12d3a8ad5b0b
Andrei Lapshin <alapshin@fastmail.com>
2025-01-20 10:25:57 +0100

    Clean-up rocm overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,6 +6,7 @@
   # When applied, the  nixpkgs set from pull-request (declared in the flake inputs)
   # will be accessible through 'pkgs.<pull-request-name>'
   development = final: _prev: {
+    rocmpr = import inputs.nixpkgs-rocm { system = final.system; };
     beancountpr = import inputs.nixpkgs-beancount3 { system = final.system; };
   };
 
@@ -13,6 +14,9 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   modifications = final: prev: {
+    ollama = prev.rocmpr.ollama;
+    rocmPackages = prev.rocmpr.rocmPackages.gfx1030;
+
     fava = prev.callPackage prev.beancountpr.fava.override { };
 
     pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
@@ -21,15 +25,14 @@
         beancount = prev.beancountpr.python3Packages.beancount3;
         autobean = pyfinal.callPackage ../packages/autobean { };
         beangulp = pyfinal.callPackage prev.beancountpr.python3Packages.beangulp.override { };
+
+        torch = pyfinal.callPackage prev.rocmpr.python3Packages.torch.override { };
       })
     ];
 
     prisma = inputs.nixpkgs-lw.legacyPackages."${prev.system}".prisma;
     lldap = inputs.nixpkgs-lldap.legacyPackages."${prev.system}".lldap;
     linkwarden = inputs.nixpkgs-lw.legacyPackages."${prev.system}".linkwarden;
-
-    ollama = inputs.nixpkgs-rocm.legacyPackages."${prev.system}".ollama;
-    rocmPackages = inputs.nixpkgs-rocm.legacyPackages."${prev.system}".rocmPackages.gfx1030;
   };
 
 }
