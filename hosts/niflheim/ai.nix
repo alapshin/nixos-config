725a35ec41b7174baf06a27dff7d0b1babc02d69
Andrei Lapshin <alapshin@fastmail.com>
2025-01-29 12:36:56 +0100

    Setup Open WebUI to use PostgreSQL

--- hosts/niflheim/ai.nix
+++ hosts/niflheim/ai.nix
@@ -5,6 +5,8 @@
   ...
 }:
 let
+  user = "open-webui";
+  database = "open-webui";
   ollamaHost = config.services.ollama.host;
   ollamaPort = config.services.ollama.port;
 in
@@ -29,19 +31,35 @@ in
       enable = true;
       port = 8085;
       environment = {
-        ENABLE_SIGNUP = "False";
-        ENABLE_LOGIN_FORM = "False";
-        ENABLE_OAUTH_SIGNUP = "True";
-        OAUTH_CLIENT_ID = "open-webui";
-        OPENID_PROVIDER_URL = "https://${config.domain.auth}/.well-known/openid-configuration";
+        DATABASE_URL = "postgresql:///${database}?host=/run/postgresql";
 
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
         ANONYMIZED_TELEMETRY = "False";
 
+        ENABLE_SIGNUP = "False";
+        ENABLE_LOGIN_FORM = "False";
+        ENABLE_OAUTH_SIGNUP = "True";
+        OAUTH_CLIENT_ID = "open-webui";
+        OAUTH_MERGE_ACCOUNTS_BY_EMAIL = "True";
+        OAUTH_ROLES_CLAIM = "groups";
+        ENABLE_OAUTH_ROLE_MANAGEMENT = "True";
+        OPENID_PROVIDER_URL = "https://${config.domain.auth}/.well-known/openid-configuration";
+
         OPENAI_API_BASE_URL = "https://api.deepinfra.com/v1/openai";
         OLLAMA_API_BASE_URL = "http://${ollamaHost}:${toString ollamaPort}";
       };
+      environmentFile = config.sops.templates."open-webui.env".path;
+    };
+
+    postgresql = {
+      ensureDatabases = [ database ];
+      ensureUsers = [
+        {
+          name = user;
+          ensureDBOwnership = true;
+        }
+      ];
     };
 
     nginx-ext.applications."owui" = {
@@ -53,7 +71,7 @@ in
 
   systemd.services = {
     open-webui.serviceConfig = {
-      EnvironmentFile = config.sops.templates."open-webui.env".path;
+      User = user;
     };
   };
 }
