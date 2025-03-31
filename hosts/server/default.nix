c9860b114ccff7139eed36bc40aa3a41f5243f39
Andrei Lapshin <alapshin@fastmail.com>
2025-03-31 09:51:48 +0200

    Add workaround to automatically generate subuids and subgids
    We are using userborn to manage users but it doesn't support
    `autoSubUidGidRange` option. To work around this limitation add small bit of
    custom logic.

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,4 +1,8 @@
-{ lib, ... }:
+{
+  lib,
+  config,
+  ...
+}:
 
 {
   imports = [
@@ -18,6 +22,7 @@
 
   # No mutable users by default
   users.mutableUsers = false;
+  services.userborn.enable = true;
 
   # No need for fonts on a server
   fonts.fontconfig.enable = lib.mkDefault false;
@@ -25,4 +30,22 @@
   environment = {
     stub-ld.enable = lib.mkDefault false;
   };
+
+  # Work-around for missing subuid support in userbon
+  # See https://github.com/nikstur/userborn/issues/7#issuecomment-2462106017
+  environment.etc =
+    let
+      autosubs = lib.pipe config.users.users [
+        lib.attrValues
+        (lib.filter (u: u.uid != null && u.autoSubUidGidRange))
+        (lib.concatMapStrings (u: "${toString u.uid}:${toString (100000 + u.uid * 65536)}:65536\n"))
+      ];
+    in
+    {
+      "subuid".text = autosubs;
+      "subuid".mode = "0444";
+      "subgid".text = autosubs;
+      "subgid".mode = "0444";
+    };
+
 }
