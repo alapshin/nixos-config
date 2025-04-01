66c60cacc2528b4e58f6fc73dd237b655991e6db
Andrei Lapshin <alapshin@fastmail.com>
2025-04-01 21:07:27 +0200

    Rename home-manager modules flake attribute

--- flake.nix
+++ flake.nix
@@ -80,9 +80,9 @@
       homeManagerConfig = import ./hm-config.nix {
         inherit dotfileDir;
         sharedModules = [
-          self.homeManagerModules.secrets
+          self.homeModules.secrets
           sops-nix.homeManagerModules.sops
-          catppuccin.homeManagerModules.catppuccin
+          catppuccin.homeModules.catppuccin
         ];
       };
 
@@ -226,7 +226,7 @@
         };
       };
 
-      homeManagerModules = import ./modules/home;
+      homeModules = import ./modules/home;
 
       # Stand-alone home-manager configuration for non NixOS machines
       homeConfigurations =
