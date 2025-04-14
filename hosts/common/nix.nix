225f1a0a1597c30b76347320185427f2dfa6d4db
Andrei Lapshin <alapshin@fastmail.com>
2025-04-14 20:24:35 +0200

    Move nix build dir to /var/tmp

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -16,6 +16,8 @@
     settings = {
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
+
+      build-dir = "/var/tmp";
       # Avoid copying unnecessary stuff over SSH
       builders-use-substitutes = true;
 
@@ -38,5 +40,6 @@
   # 100 is the default for user slices and 500 is systemd-coredumpd@
   # We rather want a build to be killed than our precious user sessions
   # as builds can be easily restarted.
+  systemd.services.nix-daemon.environment.TMPDIR = "/var/tmp";
   systemd.services.nix-daemon.serviceConfig.OOMScoreAdjust = lib.mkDefault 250;
 }
