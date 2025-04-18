3284274af86e428d68d77b57079e6012dfa3833b
Andrei Lapshin <alapshin@fastmail.com>
2025-04-18 18:24:11 +0200

    Update bean-price
    Build bean-price from git to address broken Yahoo Finance source.

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,6 +12,19 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   modifications = final: prev: {
+    beanprice = prev.beanprice.overrideAttrs {
+      version = "2.0.0-unstalbe-2025-02-25";
+      src = prev.fetchFromGitHub {
+        owner = "beancount";
+        repo = "beanprice";
+        rev = "7a6efa7e0b0c9567df27c98e3fd4e8fc3e1c7117";
+        hash = "sha256-FKKmh+xpKuV/mUtRVBHDfKVhPpQnBgUPc9UcIfkHxf8=";
+      };
+      disabledTestPaths = [
+        "beanprice/price_test.py"
+        "beanprice/sources/yahoo_test.py"
+      ];
+    };
     changedetection-io = prev.changedetection-io.overrideAttrs (oldAttrs: rec {
       version = "0.49.4";
       src = prev.fetchFromGitHub {
