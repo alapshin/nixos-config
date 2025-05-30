e558ed6311797f1b2cf673747f9920d5bb52988b
Andrei Lapshin <alapshin@fastmail.com>
2025-05-30 20:08:27 +0200

    Fix Immich OIDC setup

--- hosts/niflheim/immich.nix
+++ hosts/niflheim/immich.nix
@@ -30,16 +30,12 @@
               client_name = "Immich";
               client_secret = "$pbkdf2-sha512$310000$wkn9pc1AFRimy.bTDtULlw$ywNpmfTvmh5JP07gwLNbjH4Qa8IU5tQojXxJN2BzTxKP1r9pcbbAiik0lEgtg0uHMOMWDNZAKLg7hrHoyiPaTw";
               authorization_policy = "one_factor";
-              scopes = [
-                "email"
-                "openid"
-                "profile"
-              ];
               redirect_uris = [
                 "app.immich:///oauth-callback"
                 "https://immich.${config.services.webhost.basedomain}/auth/login"
                 "https://immich.${config.services.webhost.basedomain}/user-settings"
               ];
+              token_endpoint_auth_method = "client_secret_post";
             }
           ];
         };
