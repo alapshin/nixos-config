d67a5f8c09f8c100cf1b546a6e839697ed1265c2
Andrei Lapshin <alapshin@fastmail.com>
2025-06-06 10:14:28 +0200

    Disalbe sunshine

--- hosts/desktop/gaming.nix
+++ hosts/desktop/gaming.nix
@@ -14,7 +14,7 @@
   };
   services = {
     sunshine = {
-      enable = true;
+      enable = false;
       autoStart = true;
       capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
       openFirewall = true;
