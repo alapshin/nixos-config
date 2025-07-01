cfac65c9adb8754868ef9a669e234d878e2ff692
Andrei Lapshin <alapshin@fastmail.com>
2025-07-01 08:23:08 +0200

    Remove lxml-html-clean overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -28,9 +28,6 @@
     pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
       (pyfinal: pyprev: {
         autobean = pyfinal.callPackage ../packages/autobean { };
-        lxml-html-clean = pyprev.lxml-html-clean.overridePythonAttrs {
-          doCheck = false;
-        };
       })
     ];
 
