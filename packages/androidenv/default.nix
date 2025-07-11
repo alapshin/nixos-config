e5f85ef3331a8a6bccd9f79dd51f19d7c5459034
Andrei Lapshin <alapshin@fastmail.com>
2025-07-11 09:21:24 +0200

    Update androidenv

--- packages/androidenv/default.nix
+++ packages/androidenv/default.nix
@@ -12,12 +12,10 @@ pkgs.androidenv.composeAndroidPackages {
     "x86_64"
   ];
   platformVersions = [
-    "34"
     "35"
+    "36"
   ];
   buildToolsVersions = [
-    "30.0.3"
-    "34.0.0"
     "36.0.0"
   ];
   systemImageTypes = [ "google_apis" "google_apis_playstore" ];
