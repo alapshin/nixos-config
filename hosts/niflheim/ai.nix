af3694d846cd62fe37a10d52b691184e3ef2a8c7
Andrei Lapshin <alapshin@fastmail.com>
2025-08-04 00:06:21 +0200

    Refactor open-webui setup
    Setup redis
    Set web ui url
    Disable ollama
    Enable offline mode and disable update checks

--- hosts/niflheim/ai.nix
+++ hosts/niflheim/ai.nix
@@ -9,6 +9,7 @@ let
   database = "open-webui";
   ollamaHost = config.services.ollama.host;
   ollamaPort = config.services.ollama.port;
+  redisSocket = config.services.redis.servers."open-webui".unixSocket;
 in
 {
   sops = {
@@ -23,21 +24,17 @@ in
   };
 
   services = {
-    ollama = {
-      enable = true;
-    };
-
     open-webui = {
       enable = true;
       port = 8085;
       environment = {
         ENABLE_PERSISTENT_CONFIG = "False";
 
-        DATABASE_URL = "postgresql:///${database}?host=/run/postgresql";
-
+        OFFLINE_MODE = "True";
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
         ANONYMIZED_TELEMETRY = "False";
+        ENABLE_VERSION_UPDATE_CHECK = "False";
 
         ENABLE_SIGNUP = "False";
         ENABLE_LOGIN_FORM = "False";
@@ -48,8 +45,11 @@ in
         ENABLE_OAUTH_ROLE_MANAGEMENT = "True";
         OPENID_PROVIDER_URL = "https://${config.services.webhost.authdomain}/.well-known/openid-configuration";
 
+        REDIS_URL = "unix://${redisSocket}";
+        WEBUI_URL = "https://owui.${config.services.webhost.basedomain}";
+        DATABASE_URL = "postgresql:///${database}?host=/run/postgresql";
+
         OPENAI_API_BASE_URL = "https://openrouter.ai/api/v1";
-        OLLAMA_API_BASE_URL = "http://${ollamaHost}:${toString ollamaPort}";
       };
       environmentFile = config.sops.templates."open-webui.env".path;
     };
@@ -62,6 +62,12 @@ in
           ensureDBOwnership = true;
         }
       ];
+    };
+
+    redis.servers."open-webui" = {
+      enable = true;
+      port = 0;
+      user = user;
     };
 
     webhost.applications."owui" = {
@@ -78,6 +84,12 @@ in
               client_name = "Open WebUI";
               client_secret = "$pbkdf2-sha512$310000$b6mTChIj/dqB1tgrNWpJCA$L0o17Sn8c2U2G9U3AHmOsI03TsHIwnU9rjiqvw2hEcl/lcbc6r48cBS4aU/Bq4g9PYF9lihl3o2fbhlIOE7fEA";
               authorization_policy = "one_factor";
+              scopes = [
+                "openid"
+                "email"
+                "profile"
+                "groups"
+              ];
               redirect_uris = [ "https://owui.${config.services.webhost.basedomain}/oauth/oidc/callback" ];
             }
           ];
@@ -91,4 +103,10 @@ in
       User = user;
     };
   };
+
+  users.users."${user}" = {
+    group = user;
+    isSystemUser = true;
+  };
+  users.groups."${user}" = { };
 }
