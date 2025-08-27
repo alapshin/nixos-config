057cf8f0426b0b0569f08f8fface2b3b7b3f4604
Andrei Lapshin <alapshin@fastmail.com>
2025-08-27 14:39:33 +0200

    Remove mitmproxy overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -28,45 +28,6 @@
     pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
       (pyfinal: pyprev: {
         autobean = pyfinal.callPackage ../packages/autobean { };
-        mitmproxy = pyprev.mitmproxy.overridePythonAttrs (old: rec {
-          version = "12.1.2";
-          src = prev.fetchFromGitHub {
-            owner = "mitmproxy";
-            repo = "mitmproxy";
-            tag = "v${version}";
-            hash = "sha256-XYZ14JlVYG/OLlEze+C1L/HP3HD5GEW+jG2YYSXW/8Y=";
-          };
-          pythonRelaxDeps = [
-            "h2"
-            "passlib"
-            "urwid"
-          ];
-        });
-        mitmproxy-rs = pyprev.mitmproxy-rs.overridePythonAttrs (old: rec {
-          pname = "mitmproxy-rs";
-          version = "0.12.7";
-          src = prev.fetchFromGitHub {
-            owner = "mitmproxy";
-            repo = "mitmproxy_rs";
-            tag = "v${version}";
-            hash = "sha256-Wd/4XzSMQ3qgacFUlxReQFyonUbTqWKDCk3m+kWhXy0=";
-          };
-          cargoDeps = prev.rustPlatform.fetchCargoVendor {
-            inherit pname version src;
-            hash = "sha256-Q5EBI5uXJgbI9NMblkTT/GweopnTr/zUG35i+Aoe3QA=";
-          };
-        });
-        mitmproxy-macos = pyprev.mitmproxy-macos.overridePythonAttrs (old: rec {
-          version = "0.12.7";
-          src = prev.fetchPypi {
-            pname = "mitmproxy_macos";
-            inherit version;
-            format = "wheel";
-            dist = "py3";
-            python = "py3";
-            hash = "sha256-NArp10yhERk7Hhw5fIU+ekbupyldyzpLQdKgebiUpOM=";
-          };
-        });
       })
     ];
 
