4bf4e25ceecc20c593e0fde45881e42d4a18da19
Andrei Lapshin <alapshin@fastmail.com>
2025-08-27 21:22:42 +0200

    Fix script formatting

--- ci/build.sh
+++ ci/build.sh
@@ -49,8 +49,8 @@ function remote-switch {
 	# 	--target-host "${remote_host}" "$PWD"
 	nixos-rebuild-ng switch \
 		--no-reexec \
-        --sudo \
-        --ask-sudo-password \
+		--sudo \
+		--ask-sudo-password \
 		--build-host "${remote_host}" \
 		--target-host "${remote_host}" \
 		--flake "$PWD#${hostname}"
