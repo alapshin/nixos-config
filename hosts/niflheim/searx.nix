6d66b2a4f1502fa8b1a11f917581080940092a75
Andrei Lapshin <alapshin@fastmail.com>
2025-10-03 14:46:11 +0200

    Adjust SearXNG settings
    Enable redis
    Enable json output
    Disable botdetection

--- hosts/niflheim/searx.nix
+++ hosts/niflheim/searx.nix
@@ -16,14 +16,27 @@
   services = {
     searx = {
       enable = true;
+      redisCreateLocally = true;
       settings = {
         use_default_settings = true;
         server = {
           port = 8181;
           bind_address = "127.0.0.1";
+          method = "GET";
+          limiter = false;
           image_proxy = true;
           public_instance = false;
         };
+        search = {
+          formats = [
+            "html"
+            "json"
+          ];
+        };
+      };
+      limiterSettings = {
+        botdetection.ip_limit.link_token = false;
+        botdetection.ip_limit.filter_link_local = false;
       };
       environmentFile = config.sops.templates."searx/secret_key.env".path;
     };
