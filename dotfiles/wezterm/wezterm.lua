a5069049dea11d34842acd3c6de7e3633a0061bf
Andrei Lapshin <alapshin@fastmail.com>
2025-10-31 19:59:49 +0100

    Improve wezterm setup

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ config.default_prog = {
 }
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-config.font_size = 11
+config.font_size = 12
 config.font_rules = {
     {
         italic = false,
@@ -21,11 +21,18 @@ config.custom_block_glyphs = false
 config.bold_brightens_ansi_colors = "BrightAndBold"
 
 config.enable_tab_bar = true
-config.tab_max_width = 40
+config.tab_max_width = 48
 config.enable_scroll_bar = true
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
+
+config.window_padding = {
+    top = 0,
+    left = 0,
+    right = 0,
+    bottom = 0,
+}
 
 config.color_scheme = "Catppuccin Latte"
 
@@ -34,5 +41,15 @@ config.hyperlink_rules = wezterm.default_hyperlink_rules()
 wezterm.on("gui-startup", function(cmd)
     select(3, wezterm.mux.spawn_window(cmd or {})):gui_window():maximize()
 end)
+
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup({
+    options = {
+        theme = "Catppuccin Latte",
+        tab_separators = "",
+        section_separators = "",
+        component_separators = "",
+    },
+})
 
 return config
