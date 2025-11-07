e0ab4e3c473960f83caa29dcde918e685adb3b83
Andrei Lapshin <alapshin@fastmail.com>
2025-11-07 21:15:36 +0100

    Disable flameshot on darwin

--- users/alapshin/home/services.nix
+++ users/alapshin/home/services.nix
@@ -8,11 +8,6 @@ let
   hostname = osConfig.networking.hostName;
 in
 {
-
-  home.packages =
-    lib.lists.optionals pkgs.stdenv.hostPlatform.isDarwin [
-      pkgs.flameshot
-    ];
   services = {
     safeeyes.enable = pkgs.stdenv.hostPlatform.isLinux;
     flameshot.enable = pkgs.stdenv.hostPlatform.isLinux;
