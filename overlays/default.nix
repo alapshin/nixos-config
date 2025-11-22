0b112a9c2c18a430c0625836627ab67340067bc2
Andrei Lapshin <alapshin@fastmail.com>
2025-11-22 23:35:11 +0100

    Remove pinned open-webui

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,19 +3,19 @@
   # This one brings our custom packages from the 'packages' directory
   additions = final: prev: import ../packages { inherit final prev; };
 
-  pinned =
-    final: prev:
-    let
-      pinnedPkgs = import inputs.nixpkgs-pinned {
-        config = prev.config; # Inherit config from main nixpkgs
-        system = prev.stdenv.hostPlatform.system;
-      };
-    in
-    {
-      open-webui = pinnedPkgs.open-webui.overridePythonAttrs (oldAttrs: {
-        dependencies = oldAttrs.dependencies ++ oldAttrs.optional-dependencies.postgres;
-      });
-    };
+  # pinned =
+  #   final: prev:
+  #   let
+  #     pinnedPkgs = import inputs.nixpkgs-pinned {
+  #       config = prev.config; # Inherit config from main nixpkgs
+  #       system = prev.stdenv.hostPlatform.system;
+  #     };
+  #   in
+  #   {
+  #     open-webui = pinnedPkgs.open-webui.overridePythonAttrs (oldAttrs: {
+  #       dependencies = oldAttrs.dependencies ++ oldAttrs.optional-dependencies.postgres;
+  #     });
+  #   };
 
   # https://nixos.wiki/wiki/Overlays
   # This one contains whatever you want to overlay
@@ -23,6 +23,9 @@
   modifications = final: prev: {
     fish = prev.fish.overrideAttrs (oldAttrs: {
       doCheck = false;
+    });
+    open-webui = prev.open-webui.overridePythonAttrs (oldAttrs: {
+      dependencies = oldAttrs.dependencies ++ oldAttrs.optional-dependencies.postgres;
     });
     pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
       (pyfinal: pyprev: {
