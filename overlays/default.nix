12fcdc25608943f8b87f59f6cb77a2d731780e86
Andrei Lapshin <alapshin@fastmail.com>
2025-12-30 19:49:34 +0100

    Update fish

--- overlays/default.nix
+++ overlays/default.nix
@@ -21,6 +21,21 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   modifications = final: prev: {
+    fish = prev.fish.overrideAttrs (
+      finalAttrs: oldAttrs: rec {
+        version = "4.3.2";
+        src = prev.fetchFromGitHub {
+          owner = "fish-shell";
+          repo = "fish-shell";
+          tag = version;
+          hash = "sha256-/B4U3giKGmU5B/L5HQLS1lU8f7hsfI4aCeOjWcQ1dpA=";
+        };
+        cargoDeps = prev.rustPlatform.fetchCargoVendor {
+          inherit (finalAttrs) src patches;
+          hash = "sha256-/udRRs/ieLfazVTwM47ElExN40QdAG/OqQXmYurgC1I=";
+        };
+      }
+    );
     ghostty-bin = prev.ghostty-bin.overrideAttrs (oldAttrs: {
       src = prev.fetchurl {
         url = "https://github.com/ghostty-org/ghostty/releases/download/tip/Ghostty.dmg";
