488e2678854d92f0e667793c34751c7fcd518244
Andrei Lapshin <alapshin@fastmail.com>
2026-01-06 09:05:44 +0100

    Remove no longer needed overrides

--- overlays/default.nix
+++ overlays/default.nix
@@ -38,24 +38,12 @@
         hash = "sha256-0OYJlc7a5095u9UhXtIAqUja62rskyRClbB1eGI7PPc=";
       };
     });
-    linkwarden = inputs.nixpkgs-pinned.legacyPackages."${prev.system}".linkwarden;
     open-webui = prev.open-webui.overridePythonAttrs (oldAttrs: {
       dependencies = oldAttrs.dependencies ++ oldAttrs.optional-dependencies.postgres;
     });
     pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
       (pyfinal: pyprev: {
         autobean = pyfinal.callPackage ../packages/autobean { };
-        extract-msg = pyprev.extract-msg.overridePythonAttrs (oldAttrs: {
-          pythonRelaxDeps = [
-            "beautifulsoup4"
-          ];
-        });
-        duckdb-engine = pyprev.duckdb-engine.overridePythonAttrs (oldAttrs: {
-          doCheck = false;
-        });
-        langsmith = pyprev.langsmith.overridePythonAttrs (oldAttrs: {
-          doCheck = false;
-        });
         langchain-community = pyprev.langchain-community.overridePythonAttrs (oldAttrs: {
           doCheck = false;
         });
