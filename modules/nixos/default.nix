3f8e62206b0ed9b233a45eb67b280a33f059267b
Andrei Lapshin <alapshin@fastmail.com>
2026-01-08 20:36:56 +0100

    Move backup module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,5 +1,5 @@
 {
-  backup = import ./backup.nix;
+  backup = import ./services/backup/borgbackup.nix;
   vpn = import ./services/networking/vpn.nix;
   monica = import ./services/web-apps/monica5.nix;
   webhost = import ./services/web-server/webhost.nix;
