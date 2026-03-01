9ccf428268828f3136c2b8d61e7ca9d3f1889a10
Andrei Lapshin <alapshin@fastmail.com>
2026-03-01 11:31:09 +0100

    Use wildcard certificates with caddy

--- hosts/niflheim/caddy.nix
+++ hosts/niflheim/caddy.nix
@@ -5,7 +5,37 @@
   ...
 }:
 {
+  sops = {
+    secrets = {
+      "porkbun/api_key" = { };
+      "porkbun/secret_key" = { };
+    };
+    templates."caddy.env" = {
+      restartUnits = [ "caddy.service" ];
+      content = ''
+        PORKBUN_API_KEY=${config.sops.placeholder."porkbun/api_key"}
+        PORKBUN_API_SECRET_KEY=${config.sops.placeholder."porkbun/secret_key"}
+      '';
+    };
+  };
+
   services.caddy = {
     enable = true;
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
+      hash = "sha256-C+eTduZvOlewyzOxtTqmmWXL2yVqAq/ltq+XrzD1otY=";
+    };
+    environmentFile = config.sops.templates."caddy.env".path;
+    globalConfig = ''
+      acme_dns porkbun {
+        api_key {env.PORKBUN_API_KEY}
+        api_secret_key {env.PORKBUN_API_SECRET_KEY}
+      }
+    '';
+    # Wildcard certificate: Caddy 2.10+ reuses this cert for all individual
+    # *.bitgarage.dev virtualHosts, so they don't each trigger separate issuance.
+    virtualHosts."bitgarage.dev, *.bitgarage.dev".extraConfig = ''
+      abort
+    '';
   };
 }
