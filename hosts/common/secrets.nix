518ea5ed71efa02e363e29a0b9f4ded11b8b45b6
Andrei Lapshin <alapshin@fastmail.com>
2026-03-07 16:53:47 +0100

    Use only ed25519 ssh keys with sops-nix

--- hosts/common/secrets.nix
+++ hosts/common/secrets.nix
@@ -7,9 +7,10 @@
 
 {
   sops = {
-    age.sshKeyPaths = lib.mkDefault [
+    age.sshKeyPaths = lib.mkForce [
       "/etc/ssh/ssh_host_ed25519_key"
     ];
+    gnupg.sshKeyPaths = [];
     defaultSopsFile = lib.mkDefault ./secrets.yaml;
   };
 }
